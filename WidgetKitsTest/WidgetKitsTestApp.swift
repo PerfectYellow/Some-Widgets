7feb5507fa7883dcc10d227da3e0c3023373a262
Mohammad Afshar <neo.mohammad.afshar@gmail.com>
2024-01-17 14:44:34 +0330

    Initial Commit

---
+++ WidgetKitsTest/WidgetKitsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetKitsTestApp.swift
+//  WidgetKitsTest
+//
+//  Created by Mohammad Afshar on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WidgetKitsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
